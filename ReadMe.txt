7341ee1a4ee3f8bff8d692b5074f5fec703d6446
Vladislav Shiyanov <vladislav.shiyanov@neulion.com>
2018-04-27 17:35:58 +0700

    add read me

---
+++ ReadMe.txt
@@ -0,0 +1,8 @@
+https://vk.com/dev/methods
+
+https://vk.com/dev/messages.getDialogs
+
+https://vk-api.readthedocs.io/en/latest/search.html?q=get
+
+https://www.ibm.com/developerworks/ru/library/l-python_part_7/index.html
+https://habr.com/post/29778/
--- bot.py
+++ bot.py
@@ -1,16 +1,22 @@
 import vk_api
 
+def getMessage(val, vk):
+    vk.method('messages.get',val)
+
+def sendMessage(user_id, s, vk):
+    vk.method('messages.send',{'user_id':user_id,'messages':s})
 
 def main():
     login, password = '89528077892', 'asd456asd'
     vk_session = vk_api.VkApi(login, password)
+
+    val = {'out':0,'count':100,'time_offset':60}
 
     try:
         vk_session.auth()
     except vk_api.AuthError as error_msg:
         print(error_msg)
         return
-
     vk = vk_session.get_api()
 
     response = vk.wall.get(count=1)  # Используем метод wall.get
@@ -18,5 +24,14 @@ def main():
     if response['items']:
         print(response['items'][0])
 
+
+    #sendMessage('vlad.shianov','huui',vk)
+    #getMessage(vk,val)
+    res = vk.messages.getDialogs()
+    if res['items']:
+        print(res['items'][0])
+
+
+        
 if __name__ == '__main__':
     main()
